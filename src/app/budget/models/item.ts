228007ef66fd55ada54d8f964074b9bd255e21e9
Yuriekokubu <60240233118@udru.ac.th>
2024-10-12 14:33:16 +0700

    day 3

---
+++ src/app/budget/models/item.ts
@@ -0,0 +1,13 @@
+// item.ts
+export enum ItemStatus {
+    PENDING = 'PENDING', APPROVED = 'APPROVED', REJECTED = 'REJECTED'
+  }
+  
+  export interface Item {
+    id: number;
+    title: string;
+    amount: number;
+    price: number;
+    contactMobileNo: string;
+    status: ItemStatus;
+  }
